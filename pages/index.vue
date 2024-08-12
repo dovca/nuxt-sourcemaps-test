50b67461d39447793f967d6e21ff4c6fae495eea
David Ovčačík <david.ovcacik@gmail.com>
2024-08-12 16:22:36 +0200

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+
+</template>
+
+<script setup lang="ts">
+	import throwTest from '~/throwTest';
+
+	throwTest();
+</script>
---
+++ throwTest.ts
@@ -0,0 +1,3 @@
+export default function throwTest() {
+	throw new Error('TEST');
+}
